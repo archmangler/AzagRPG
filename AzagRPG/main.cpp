b3228b673c628888031b3e6efaab90e0f7b3dc1a
Traiano Welcome <traiano@gmail.com>
2024-02-07 15:26:13 +0800

    Initial Commit

---
+++ AzagRPG/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AzagRPG
+//
+//  Created by Traiano on 7/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
